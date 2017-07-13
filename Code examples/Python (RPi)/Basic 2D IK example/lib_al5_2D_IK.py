95f9decde5cff9f2bf4f705358adf61c16d57280
rbscharette <scharette@robotshop.com>
2017-07-13 14:37:58 -0400

    Added a basic 2D IK example

---
+++ Code examples/Python (RPi)/Basic 2D IK example/lib_al5_2D_IK.py
@@ -0,0 +1,122 @@
+from math import sqrt, atan, acos, fabs
+
+# Define legnth of AL5 sections
+# if AL5A
+#A = 3.75;
+#B = 4.25;
+
+#if AL5B
+#A = 4.75;
+#B = 5.00;
+
+#if AL5D
+A = 5.75;
+B = 7.375;
+
+# Constants
+rtod = 57.295779	# Radians to degrees constant
+CST_ANGLE_MIN = 0
+CST_ANGLE_MAX = 180
+CST_RC_MIN = 500
+CST_RC_MAX = 2500
+
+def ard_constrain(value, min, max):
+	if(value < min):
+		value = min
+	if(value > max):
+		value = max
+	return (value)
+
+def ard_map(value, min1, max1, min2, max2):
+	value = ((value - min1) * (max2 - min2) / (max1 - min1) + min2);
+	return (value)
+
+def getPulseFromAngle(angle):
+	angle = ard_constrain(angle, CST_ANGLE_MIN, CST_ANGLE_MAX)
+	pulse = ard_map(angle, CST_ANGLE_MIN, CST_ANGLE_MAX, CST_RC_MIN, CST_RC_MAX)
+	return (pulse);
+
+#def al5_2D_IK(targetX, targetY, targetZ, targetGrip, targetWAgl, targetWRot):
+def al5_2D_IK(targetXYZGWAWR):
+
+	# Initialize variables
+	x = targetXYZGWAWR[0]
+	y = targetXYZGWAWR[1]
+	z = targetXYZGWAWR[2]
+	g = targetXYZGWAWR[3]
+	wa = targetXYZGWAWR[4]
+	wr = targetXYZGWAWR[5]
+	Elbow = 0
+	Shoulder = 0
+	Wrist = 0
+	
+	# Get distance
+	floatM = sqrt((y * y) + (x * x))
+#	print("floatM        = " + str(floatM))
+	
+	# Check X position for error
+	if(floatM <= 0):
+		return 1
+	
+	# Get first angle (radians)
+	floatA1 = atan(y / x)
+#	print("floatA1       = " + str(floatA1))
+#	print("x             = " + str(x))
+	
+	# Check X position for error
+	if(x <= 0):
+		return 2
+	
+	# Get 2nd angle (radians)
+	floatA2 = acos((A * A - B * B + floatM * floatM) / ((A * 2) * floatM))
+#	print("floatA2       = " + str(floatA2))
+	
+	# Calculate elbow angle (radians)
+	floatElbow = acos((A * A + B * B - floatM * floatM) / ((A * 2) * B))
+#	print("floatElbow    = " + str(floatElbow))
+	
+	# Calculate shoulder angle (radians)
+	floatShoulder = floatA1 + floatA2
+#	print("floatShoulder = " + str(floatShoulder))
+	
+	# Obtain angles for shoulder / elbow
+	Elbow = floatElbow * rtod
+#	print("Elbow         = " + str(floatA2))
+	Shoulder = floatShoulder * rtod
+#	print("Shoulder      = " + str(Shoulder))
+	
+	# Check elbow/shoulder angle for error
+	if((Elbow <= 0) or (Shoulder <= 0)):
+		return 3
+	Wrist = fabs(wa - Elbow - Shoulder) - 90
+	
+	# Return the new values
+	motors_SEWBZWrG = (Shoulder, Elbow, Wrist, z, g, wr)
+	
+	# <<< debug <<<
+	#print("SHOULDER\tELBOW   \tWRIST-A \tBASE-ROT\tGRIPPER \tWRIST-R \t")
+	#print(str(Shoulder) + "\t" + str((180 - Elbow)) + "\t" + str((180 - Wrist)) + "\t" + str(z) + "\t" + str(g) + "\t" + str(wr) + "\t")
+	#print(str(getPulseFromAngle(Shoulder)) + "\t" + str(getPulseFromAngle(180 - Elbow)) + "\t" + str(getPulseFromAngle(180 - Wrist)) + "\t" + str(getPulseFromAngle(z)) + "\t" + str(getPulseFromAngle(g)) + "\t" + str(getPulseFromAngle(wr)) + "\t")
+	# >>> debug >>>
+	
+	return (motors_SEWBZWrG)
+
+def al5_moveMotors(motors_SEWBZWrG, serial):
+	
+	# Get values from angles to pulses (µs)
+	pulseShoulder = getPulseFromAngle(motors_SEWBZWrG[0])
+	pulseElbow = getPulseFromAngle((180 - motors_SEWBZWrG[1]))
+	pulseWrist = getPulseFromAngle((180 - motors_SEWBZWrG[2]))
+	pulseZ = getPulseFromAngle(motors_SEWBZWrG[3])
+	pulseG = getPulseFromAngle(motors_SEWBZWrG[4])
+	pulseWR = getPulseFromAngle(motors_SEWBZWrG[5])
+	
+	# Write values to SSC-32U
+	serial.write(("#0 P" + str(pulseZ) + "\r").encode())
+	serial.write(("#1 P" + str(pulseShoulder) + "\r").encode())
+	serial.write(("#2 P" + str(pulseElbow) + "\r").encode())
+	serial.write(("#3 P" + str(pulseWrist) + "\r").encode())
+	serial.write(("#4 P" + str(pulseG) + "\r").encode())
+	serial.write(("#5 P" + str(pulseWR) + "\r").encode())
+	
+	return
---
+++ Code examples/Python (RPi)/Basic 2D IK example/test.py
@@ -0,0 +1,89 @@
+# 2017-07-12 by scharette
+# Basic 2D IK example of AL5 + Python
+
+# Obtain required libraries
+import serial
+
+from lib_al5_2D_IK import al5_2D_IK, al5_moveMotors
+
+# Create and open a serial port
+sp = serial.Serial('/dev/ttyUSB0', 9600)
+
+# Set default values
+AL5_DefaultPos = 1500;
+cont = True
+defaultTargetX = 4
+defaultTargetY = 4
+defaultTargetZ = 90
+defaultTargetG = 90
+defaultTargetWA = 0
+defaultTargetWR = 90
+defaultTargetShoulder = 90
+defaultTargetElbow = 90
+targetX = defaultTargetX
+targetY = defaultTargetY
+targetZ = defaultTargetZ
+targetG = defaultTargetG
+targetWA = defaultTargetWA
+targetWR = defaultTargetWR
+index_X = 0
+index_Y = 1
+index_Z = 2
+index_G = 3
+index_WA = 4
+index_WR = 5
+targetXYZGWAWR = (targetX, targetY, targetZ, targetG, targetWA, targetWR)
+targetQ = "y"
+
+# Set the arm to default centered position (careful of sudden movement)
+print("Default position is " + str(AL5_DefaultPos) + ".")
+for i in range(0,6):
+	print(("#" + str(i) + " P" + str(AL5_DefaultPos) + "\r").encode())
+	sp.write(("#" + str(i) + " P" + str(AL5_DefaultPos) + "\r").encode())
+
+while cont:
+	
+	# Get X/Y position of end effector and perform IK on it
+	print("")
+	print("--- --- --- --- --- --- --- --- --- ---")
+	print("< Set X/Y position of end effector >")
+	print("")
+	
+	# Get X position
+	targetInput = input("X position (last X = " + str(targetXYZGWAWR[index_X]) + ") ? ")
+	if(targetInput == ""):
+		targetX = defaultTargetX;
+	else:
+		targetX = int(targetInput);
+	targetXYZGWAWR = (targetX, targetXYZGWAWR[1], targetXYZGWAWR[2], targetXYZGWAWR[3], targetXYZGWAWR[4], targetXYZGWAWR[5])
+	
+	# Get Y position
+	targetInput = input("Y position (last Y = " + str(targetXYZGWAWR[index_Y]) + ") ? ")
+	if(targetInput == ""):
+		targetY = defaultTargetY;
+	else:
+		targetY = int(targetInput);
+	targetXYZGWAWR = (targetXYZGWAWR[0], targetY, targetXYZGWAWR[2], targetXYZGWAWR[3], targetXYZGWAWR[4], targetXYZGWAWR[5])
+	
+	# Perform IK
+	errorValue = al5_2D_IK(targetXYZGWAWR)
+	if isinstance(errorValue, tuple):
+		motors_SEWBZWrG = errorValue
+	else:
+		print(errorValue)
+		motors_SEWBZWrG = (defaultTargetShoulder, defaultTargetElbow, defualtTargetWA, defaultTargetZ, defaultTargetG, defaultTargetWR)
+	
+	# Move motors
+	errorValue = al5_moveMotors(motors_SEWBZWrG, sp)
+	
+	# Quit? (quit on "y", continue on any other input)
+	targetQ = str(input("Quit ? (Y/N) "))
+	if targetQ == "y":
+		cont = False
+
+# Set all motors to idle/unpowered (pulse = 0)
+print("< Idling motors... >");
+for i in range(0,6):
+	print(("#" + str(i) + " P" + str(0) + "\r").encode())
+	sp.write(("#" + str(i) + " P" + str(0) + "\r").encode())
+print("< Done >")
